c846f882a2ef3a47ee2fc091b238d7af68b15165
davide <davide.serramazza@gmail.com>
2023-10-06 12:35:20 +0100

    #TODO refactor main

--- train_models.py
+++ train_models.py
@@ -1,6 +1,6 @@
-from dCAM.src.models.CNN_models import ResNetBaseline, ModelCNN
-from utils import transform_data4ResNet
-from models.MyMiniRocket import MyMiniRocket
+from models.dCAM.src.models.CNN_models import ResNetBaseline,dResNetBaseline, ModelCNN
+from pytorch_utils import transform_data4ResNet
+from models.MyModels.MyMiniRocket import MyMiniRocket
 from load_data import load_data
 from utils import MyDataset
 import torch
@@ -12,12 +12,12 @@ def main():
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    train_X, train_y,test_X,test_y, seq_len, n_channels, n_classes = load_data("synth_2lines")
+    train_X, train_y,test_X,test_y, seq_len, n_channels, n_classes = load_data("CMJ")
 
-
+    """
     miniRocket = MyMiniRocket("MiniRocket",n_channels,seq_len,n_classes)
-    X_train = torch.tensor( train_X ).to(device)
-    X_test = torch.tensor( test_X ).to(device)
+    X_train = torch.tensor( train_X ).type(torch.float).to(device)
+    X_test = torch.tensor( test_X ).type(torch.float).to(device)
 
     start = timeit.default_timer()
     X_train, X_test = miniRocket.transform_dataset(X_train=X_train,X_test=X_test, chunk_size=512,normalise=True)
@@ -33,17 +33,18 @@ def main():
     #probs = miniRocket( torch.Tensor( data['test']['X'] ). to("cuda")  )
     print("classifier in ", timeit.default_timer() - start)
     #torch.save(miniRocket,"saved_models/Rocket_1line_CS10.pt")
+    """
 
 
-    exit()
 
 
     train_loader, test_loader,n_channels,n_classes, device, enc= (
         transform_data4ResNet(train_X,train_y,test_X,test_y,device=device))
-    resNet = ResNetBaseline(n_channels, mid_channels=128,num_pred_classes= n_classes).to(device)
-    model = ModelCNN(model=resNet, n_epochs_stop=30,device=device)
-    acc = model.train(num_epochs=200,train_loader=train_loader,test_loader=test_loader)
+    resNet = dResNetBaseline(n_channels, mid_channels=128,num_pred_classes= n_classes).to(device)
+    model = ModelCNN(model=resNet, n_epochs_stop=50,device=device)
+    acc = model.train(num_epochs=300,train_loader=train_loader,test_loader=test_loader)
     print("resNet accuracy was ",acc)
+    exit()
     #torch.save(model,"saved_models/resNet128_1line.pt")
 
     train_X, train_y,test_X,test_y, seq_len, n_channels, n_classes = load_data("tmp_synth_2lines", concat=True)
