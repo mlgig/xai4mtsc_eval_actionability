9bbeed0f53181d182fe8cbf94e6f2aa9e8446db2
davide <davide.serramazza@gmail.com>
2023-10-19 10:09:50 +0100

    captum extended #TODO lot of refactoring to be done!

--- captum_methods.py
+++ captum_methods.py
@@ -1,3 +1,6 @@
+import os
+import numpy as np
+from tqdm import trange
 import torch
 from load_data import load_data
 from captum.attr import (
@@ -11,52 +14,112 @@ from captum.attr import (
     NoiseTunnel,
     Saliency,
     ShapleyValueSampling,
-    FeaturePermutation
+    FeaturePermutation,
+    FeatureAblation
 )
 from pytorch_utils import transform_data4ResNet, transform2tensors
 import timeit
 
+captum_mthods = [
+    {"method" :ShapleyValueSampling, "require_baseline":False, "batch_size":4},
+    #{"method" :DeepLift, "require_baseline":False, "batch_size":16},
+    #{"method" :DeepLiftShap, "require_baseline":True, "batch_size":16},
+    #{"method" :IntegratedGradients, "require_baseline":False, "batch_size":4},
+    ##{"method" :GradientShap, "require_baseline":True, "batch_size":16},
+    #{"method" :Saliency, "require_baseline":False, "batch_size":16},
+    #{"method" :KernelShap, "require_baseline":False, "batch_size":1},
+    #{"method" :Lime, "require_baseline":False, "batch_size":1},
+    #{"method" : FeatureAblation, "require_baseline":False, "batch_size":8},
+    #{"method" : FeaturePermutation, "require_baseline":False, "batch_size":16},
+]
+
+def get_grouping4dResNet(sample):
+    groups = torch.tensor( [i for i in range( sample.size)])
+    # TODO get why it's not working
+    return groups.resize(1,8,500)
 
 def main():
 
     # set device, load and transform data
-    device = "cpu"# "cuda" if torch.cuda.is_available() else "cpu"
-    X_train,y_train,X_test,y_test, seq_len, n_channels, n_classes = load_data("CMJ")
-    X_train,y_train,X_test,y_test, enc = transform2tensors(X_train,y_train,X_test,y_test,device=device)
+    device ="cuda" if torch.cuda.is_available() else "cpu"
 
-    # loading the models
-    for saved_model_name in ["MiniRocket",  "ResNet",  "Rocket"]:
-        model = torch.load("saved_models/CMJ/"+saved_model_name+".pt",
-            map_location=device) # using the map_location argument is possible to freely load your model in CPU or GPU
-            # regardless of the device used to train it
-        baseline = torch.normal(mean=0, std=1, size=X_test[:1].shape).to(device)
-        expected_value = model(baseline.to(device))
-        kernel_shap = KernelShap(model)
+    # TODO for loop for every dataset?
+    dataset_name="synth_2lines"
+    X_train,y_train,X_test,y_test, seq_len, n_channels, n_classes = load_data(dataset_name)
 
-        for i in range(3):
-            samples = X_test[i:(i+1)]
-            labels =  y_test[i:(i+1)]       # labels has to be torch.int64 (also called torch.long)
+    for model_name in [ "ResNet.pt", ]:     # "dResNet_64_0_910.pt"
+        model = torch.load( os.path.join( "saved_models" ,dataset_name ,model_name) )
+
+        if model_name.startswith("ResNet"):
+            X_train,y_train,X_test,y_test, enc = transform2tensors(X_train,y_train,X_test,y_test,device=device)
+            # loading the model
+        elif model_name.startswith("dResNet"):
+            sample = X_train[0]
+            X_train,y_train,X_test,y_test, enc = transform_data4ResNet(X_train,y_train,X_test,y_test,device=device, batch_s=None)
+            groups = get_grouping4dResNet(sample)
+            # TODO only test mode!
+            groups = transform_data4ResNet(groups,
+                    torch.tensor([0]),groups,torch.tensor( [0]),device=device, batch_s=None)[0].type(torch.int64).to(device)
+
+
+        baseline = torch.normal(mean=0, std=1, size=X_test[:2].shape).to(device)
+        #expected_value = model(baseline.to(device))
+
+        attributions = {}
+        # TODO do not forget about ShapleyValueSampling !
+        for method_dict  in captum_mthods:
+            method_name = str(method_dict["method"]).split(".")[-1][:-2]
+
+            explainer = method_dict["method"](model)
+            batch_size = method_dict["batch_size"]
+            explanations = []
 
             start = timeit.default_timer()
 
-            # explaining all points in all channels
-            explanation = kernel_shap.attribute(samples, target=labels, baselines=baseline)
+            for i in trange(0,100,batch_size):
+                # TODO exactly 100 samples!
+                samples = ( X_test[i:(i+batch_size )] ).clone().detach().requires_grad_(True).to(device)
+                labels =  y_test[i:(i+batch_size )].clone().detach().to(device) # labels has to be torch.int64 (also called torch.long)
+
+                # TODO use kwargs!
+                # explaining all points in all channels
+
+                if method_dict["require_baseline"]:
+                    if  model_name.startswith("dResNet"):
+                        explanation = explainer.attribute(samples, target=labels, baselines=baseline,feature_mask=groups)
+                    else:
+                        explanation = explainer.attribute(samples, target=labels, baselines=baseline)
+
+                else:
+                    if  model_name.startswith("dResNet"):
+                        explanation = explainer.attribute(samples, target=labels,feature_mask=groups)
+                    else:
+                        groups = torch.tensor(  [ [i+j for i in range(10) ] for j in range(8)]).type(torch.long)
+                        groups = torch.repeat_interleave(groups,50,dim=1).to(device)
+                        explanation = explainer.attribute(samples, target=labels,feature_mask=groups)#, baselines=baseline)
+                # in any case
+                explanations.append(explanation.cpu().detach().numpy())
+
             end = timeit.default_timer()
-            print( "explaining the instance ", i, "-th,using model ",saved_model_name, "took",(end-start), " seconds\n",
+            attributions[method_name] = np.concatenate(explanations)
+            print( "explaining the instance ", i, " using ", method_dict["method"] , "took",(end-start), " seconds\n",
                    "in shape:",samples.shape,"out shape:", explanation.shape,"\n")
 
+        # TODO save in a separate folder
+        np.save("explanations100.npy",attributions)
+        """
+        # explainin in chunks: chunk 0 from 0 t 100
+        grouping = torch.zeros(1,3,384).type(torch.int64)
+        grouping[:,:,100:200] = 1   #chunk 1 from 100 t 200
+        grouping[:,:,200:300] = 2   #chunk 2 from 200 t 300
+        grouping[:,:,300:] = 3      #chunk 3 from 300 on
 
-            # explainin in chunks: chunk 0 from 0 t 100
-            grouping = torch.zeros(1,3,384).type(torch.int64)
-            grouping[:,:,100:200] = 1   #chunk 1 from 100 t 200
-            grouping[:,:,200:300] = 2   #chunk 2 from 200 t 300
-            grouping[:,:,300:] = 3      #chunk 3 from 300 on
-
-            start = timeit.default_timer()
-            explanation_grouped = kernel_shap.attribute(samples, target=labels, baselines=baseline, feature_mask=grouping)
-            end = timeit.default_timer()
-            print( "grouped explanation: in shape:", samples.shape, "out shape:", explanation_grouped.shape,
-                   " but n. different elements", len(torch.unique(explanation_grouped)), "time was ", (end-start) ),"\n\n\n"
+        start = timeit.default_timer()
+        explanation_grouped = kernel_shap.attribute(samples, target=labels, baselines=baseline, feature_mask=grouping)
+        end = timeit.default_timer()
+        print( "grouped explanation: in shape:", samples.shape, "out shape:", explanation_grouped.shape,
+               " but n. different elements", len(torch.unique(explanation_grouped)), "time was ", (end-start) ),"\n\n\n"
+        """
 
 if __name__ == "__main__" :
     main()
