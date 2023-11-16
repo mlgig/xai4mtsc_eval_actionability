4e5b48939da153fc5232a4feeeefc05ce7270148
davide <davide.serramazza@gmail.com>
2023-11-16 16:07:14 +0000

    it seems to work now!

--- compare_explanations.py
+++ compare_explanations.py
@@ -1,180 +1,64 @@
 import os
 import numpy as np
-from sklearn.metrics import (precision_recall_fscore_support,
-                             confusion_matrix,average_precision_score, roc_auc_score,
-                             multilabel_confusion_matrix,roc_auc_score,average_precision_score)
+from sklearn.metrics import ( roc_auc_score, average_precision_score)
 from utils import minMax_normalization
+from load_data import load_data
 
-
-threshold = 0.5
-# TODO delete limit!
-limit= 200
 
 def add_random_explanation(data):
-    for model in data:
-        # TODO not hardcoded!
-        # TODO modify cpatum_method such that it only saves one row for dResNet!
-        random = np.random.normal(0,1,size=(7500,8,500))
+	for model in data:
+		first_method = list(data[model]["explanations"].keys())[0]
+		shape = data[model]["explanations"][first_method].shape
+		random = np.random.normal(0, 1, size=shape)
+		data[model]["explanations"]["random"] = random
 
-        data[model]["explanations"]["random"] = random
-    a = 2
-    return data
-
-def select_first_row(explanations):
-    return explanations[:,0,:,:]
-
-def common_correct_classified(data):
-    correct_classified = np.array( [i for i in range(7500)] )
-
-    # TODO get what is the problem here
-    for model in data:
-
-        current_correct_classified = \
-            np.where(data[model]['ground_truth_labels']==data[model]['predicted_labels'])[0]
-        correct_classified = np.intersect1d(correct_classified,current_correct_classified)
-
-    return correct_classified
+	return data
 
 
+def compute_metrics(gts, exps, model, exp_name):
+	exps_normalized = []
+	gts_toAnlyzed = []
+	for i in range(gts.shape[0]):
+		exps_normalized.append(minMax_normalization(exps[i]).flatten())
+		gts_toAnlyzed.append(gts[i].flatten())
+	exps_normalized = np.array(exps_normalized)
+	gts_toAnlyzed = np.array(gts_toAnlyzed)
 
-def compute_metrics(gts,exps, model, exp_name, toAnalyze,X):
+	roc = roc_auc_score(gts_toAnlyzed, exps_normalized, average="samples")
+	ap = average_precision_score(gts_toAnlyzed, exps_normalized, average="samples")
+	print(model, "\t", exp_name, "\t", ap, "\t", roc)
 
-    exps_normalized = []
-    gts_toAnlyzed = []
-    for i in range(toAnalyze.shape[0]):
-        current_idx = toAnalyze[i]
-        exps_normalized.append( minMax_normalization(exps[current_idx]).flatten() )
-        gts_toAnlyzed.append( gts[current_idx].flatten() )
-    exps_normalized = np.array( exps_normalized ) ; gts_toAnlyzed = np.array(gts_toAnlyzed)
-
-    roc = roc_auc_score(gts_toAnlyzed,exps_normalized,average="samples")
-    ap =  average_precision_score( gts_toAnlyzed, exps_normalized, average="samples")
-    print(model,"\t", exp_name,"\t",ap,"\t",roc)
-    """
-    i = 0
-    precision=0; recall = 0 ; f1 = 0
-    roc = 0 ; ap= 0
-    j=0 
-    while i < limit:
-        # preprocessing gts and preds
-        try:
-            current_idx = toAnalyze[i]
-            current_gt = gts[current_idx].flatten()
-
-            current_exp = minMax_normalization(exps[current_idx]).flatten()
-
-            importants = (current_exp>=threshold).astype(int)
-
-            #### compute precisioon, recall and f1 score
-            curr_precision, curr_recall, _, support = precision_recall_fscore_support(current_gt,importants
-                    ,average="binary", labels=[0,1])
-            precision += curr_precision; recall+=curr_recall
-
-            ## compute curves
-            roc += roc_auc_score(current_gt,current_exp,average="samples")
-            ap += average_precision_score(current_gt,current_exp,average="samples")
-        except ValueError:
-            print("error at index ",current_idx)
-
-        i+=1
-    
-    roc /= limit ; ap/=limit
-    precision /= limit ; recall /= limit ;
-    f1 = 2*(precision*recall) / (precision+recall)
-    #print(model+ " \t"+exp_name+"\t {:.3f} \t {:.3f} \t{:.3f} \t{:.3f} \t{:.3f}".format(precision,
-    #                                                                                    recall,f1,ap,roc) )
-    print(model+ " \t"+exp_name+"\t {:.3f} \t {:.3f} ".format(ap,roc) )
-    """
 
 def main():
-    exp_base_path = "./explanations/"
-    datasets = os.listdir(exp_base_path)
-    for dataset_name in ["synth_2lines"]: #["synth_1line", "synth_2lines"]:
+	exp_base_path = "./explanations/"
+	datasets = os.listdir(exp_base_path)
+	for dataset_name in ["synth_2lines"]:  # ["synth_1line", "synth_2lines"]:
 
-        # load dataset
-        data_base_path = "./datasets/synthetics/"
-        dataset = np.load( os.path.join( data_base_path,dataset_name+".npy" ) ,allow_pickle=True ).item()
-        exp_ground_truths = dataset["test"]["ground_truth"]
-        X =  dataset["test"]["X"]
+		# load dataset
+		# TODO what to do with the following line
+		data_base_path = "./datasets/synthetics/"
+		test_X, test_y, exp_ground_truths, seq_len, n_channels, n_classes = load_data(dataset_name, explanation_gt=True)
 
-        # load all the explanations for a single model
-        models = os.listdir( os.path.join( exp_base_path,dataset_name ))
-        # TODO delete
-        models.remove("all_items");  models.remove("old_chunks"),# models.remove("tmp"); models.remove("chunks")
-        data = {}
+		# TODO delete
+		exp_ground_truths = np.concatenate( ( exp_ground_truths[:50], exp_ground_truths[-50:] ))
 
-        for model in models:
-            # load all the explanations for the current model
-            data[model] = np.load( os.path.join( exp_base_path, dataset_name, model ) , allow_pickle=True ).item()
+		# load all the explanations for a single model
+		models = os.listdir(os.path.join(exp_base_path, dataset_name))
+		data = {}
 
-        toAnalyze = common_correct_classified(data)
-        data = add_random_explanation(data)
+		for model in models:
+			# load all the explanations for the current model
+			data[model] = np.load(os.path.join(exp_base_path, dataset_name, model), allow_pickle=True).item()
 
-        for model in data:
-            for current_exps_name in data[model]["explanations"].keys():
-                if current_exps_name.lower()=="lime":
-                    continue
-                current_exps = data[model]["explanations"][current_exps_name]
-                compute_metrics(exp_ground_truths,current_exps,model ,current_exps_name, toAnalyze,X)
-            print("\n\n\n")
+		data = add_random_explanation(data)
 
-if __name__ == "__main__" :
-    main()
+		for model in data:
+			for current_exps_name in data[model]["explanations"].keys():
+				if current_exps_name.lower() != "lime":
+					current_exps = data[model]["explanations"][current_exps_name]
+					compute_metrics(exp_ground_truths, current_exps, model, current_exps_name)
+			print("\n\n\n")
 
 
-
-"""
-    for i in range(limit):
-        current_gt = gts[i,:,:]
-        current_exp = minMax_normalization( exps[i,:,: ])
-        importants = (current_exp>=treshold).astype(np.int32)
-
-
-        #current_gt = current_gt.flatten() ; importants = importants.flatten()
-        precision, recall, f1,support = precision_recall_fscore_support(
-            current_gt,importants,average="binary", pos_label=1)
-        a = 2
-"""
-
-
-
-"""
-	# micro precision, recall and f1
-	confusion_matrices = multilabel_confusion_matrix(gts,importants)
-	tot_tn, tot_fp, tot_fn, tot_tp = np.sum(confusion_matrices, axis=0).ravel()
-	micro_precision = tot_tp / (tot_tp + tot_fp)
-	micro_recall = tot_tp / (tot_tp + tot_fn)
-	micro_f1 = 2*(micro_precision*micro_recall) / (micro_precision+micro_recall)
-
-	# samples  precision, recall and f1
-	confusion_matrices = multilabel_confusion_matrix(gts,importants,samplewise=True)
-	sample_precision = 0 ; sample_recall = 0 ; sample_f1 = 0
-	for i in range(limit):
-		tn, fp, fn, tp = confusion_matrices[i].ravel()
-		sample_precision += tp / (tp+fp)
-		sample_recall += tp / (tp+fn)
-	sample_precision /= limit ; sample_recall /= limit
-	sample_f1 = (2* sample_precision * sample_recall) / (sample_precision+sample_recall)
-
-	i+=1
-
-roc_auc_micro = roc_auc_score(gts,exps,average="micro")
-roc_auc_samples = roc_auc_score(gts,exps,average="samples")
-
-
-ap_auc_micro = average_precision_score(gts,exps,average="micro")
-ap_auc_samples = average_precision_score(gts,exps,average="samples")
-
-
-# TODO return results when the metrics to be used are selected
-#print("micro precision, recall and f1 for",model, " are: ",micro_precision,micro_recall,micro_f1,"\n",
-#       "sample precision, recall and f1 for",model, " are: ",sample_precision,sample_recall,sample_f1,"\n",
-#   "roc auc, samples/micro average are",roc_auc_samples,roc_auc_micro,
-#      "ap auc, samples/micro average are", ap_auc_samples,ap_auc_micro,"\n")
-#print(model, exp_name, sample_precision, sample_recall, sample_f1,roc_auc_samples, ap_auc_samples )
-print(model+ " \t"+exp_name+"\t {:.3f} \t {:.3f} \t{:.3f} \t{:.3f} \t{:.3f}".format(0,roc,
-			ap,ap_auc_samples,roc_auc_samples))
-#tmp = multilabel_confusion_matrix(gts,importants,labels=[1])
-#print(np.sum(confusion_matrices,axis=0),tmp, "\n\n")
-return 2
-"""
+if __name__ == "__main__":
+	main()
