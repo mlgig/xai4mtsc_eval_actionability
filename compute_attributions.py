a8e1efcce485f842d1e02349ca90305a9958dd51
davide <davide.serramazza@gmail.com>
2024-04-10 15:22:19 +0100

    get_groups function edited

--- compute_attributions.py
+++ compute_attributions.py
@@ -57,12 +57,14 @@ def load_dataset(device, groups, model_name, test_X, test_y, train_X, train_y):
 
 def get_groups(n_chunks,n_channels,series_length):
     """
-    function to get the grouping of time points into time Series accordingly to the provided number of chunks
-    :param n_chunks:        number of chunks to be used
-    :param n_channels:      number of channel of each instance in the dataset
-    :param series_length:   length of each channel of each instance in the dataset
-    :return:                a numpy array representing how to group the time points
-    """
+	function returning how to group time points into time Series accordingly to the given arguments
+	To be noted that it operates channel-wise i.e. each channel is divided into "n_chunks" chunks
+
+	:param n_chunks:        number of chunks to be used
+	:param n_channels:      number of channel of each instance in the dataset
+	:param series_length:   length of each channel of each instance in the dataset
+	:return:                a numpy array representing how to group the time points
+	"""
     groups = np.array([[i + j * n_chunks for i in range(n_chunks)] for j in range(n_channels)])
     groups = np.expand_dims(np.repeat(groups, np.ceil(series_length / n_chunks).astype(int), axis=1), 0)[:, :, :series_length]
     return groups
